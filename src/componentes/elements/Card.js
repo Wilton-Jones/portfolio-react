b59437148afe3b3664ffb6f0468f8c2fe4a83923
Wilton-Jones <wilton.jones@hotmail.com>
2024-07-13 09:44:37 -0300

    Versao 02

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,20 +1,34 @@
 import styles from "./Card.module.css";
 import ButtonB from "./ButtonB";
+import { useState } from "react";
 
 function Card({ img, title, tech, description, repository, site }) {
+  const [info, setInfo] = useState(false);
+
+  function infoOn() {
+    setInfo(true);
+  }
+
+  function infoOff() {
+    setInfo(false);
+  }
+
   return (
-    <div className={styles.card}>
-      <a target="blank" href={site}>
+    <div onMouseLeave={infoOff} className={styles.card}>
+      <a onMouseEnter={infoOn} target="blank" href={site}>
         <img src={img} alt="Error!" />
       </a>
-      <div className={styles.cardInfo}>
-        <h3>{title}</h3>
-        <p>
-          <strong>Tecnologias:</strong> {tech}
-        </p>
-        <p>{description}</p>
-        <ButtonB text="Acesse o repositório" link={repository} />
-      </div>
+
+      {info === true && (
+        <div className={styles.cardInfo}>
+          <h3>{title}</h3>
+          <p>
+            <strong>Tecnologias:</strong> {tech}
+          </p>
+          <p>{description}</p>
+          <ButtonB text="Acesse o repositório" link={repository} />
+        </div>
+      )}
     </div>
   );
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,13 +1,48 @@
 import styles from "./Presentation.module.css";
 import ButtonA from "../elements/ButtonA";
+import { useState, useEffect } from "react";
 
 function Presentation() {
+  const [text, setText] = useState("");
+  const toRotate = ["Wilton Jones", "Desenvolvedor Full Stack"];
+  const [loop, setLoop] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [delta, setDelta] = useState();
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      toType();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const toType = () => {
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(50);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setDelta(200);
+      setLoop(loop + 1);
+    }
+  };
+
   return (
     <div className={styles.presentation} id="Presentation">
       <h3>
         <strong>Bem-vindo ao meu Portfólio</strong>
       </h3>
-      <h1>Olá, eu sou Wilton Jones</h1>
+      <h1>Olá, eu sou {text}</h1>
       <p>
         Sou um apaixonado por tecnologia e soluções inovadoras.Nesse momento{" "}
         <br />
